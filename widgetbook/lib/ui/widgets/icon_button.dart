f0216150b4c72fc4dae5a438b312f7cff2237d5f
idilnsk <idilcalislar@gmail.com>
2024-12-14 16:44:43 +0100

    fix:primaryButton

--- widgetbook/lib/ui/widgets/icon_button.dart
+++ widgetbook/lib/ui/widgets/icon_button.dart
@@ -1,4 +1,20 @@
-<<<<<<< HEAD
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:groceries_app/ui/ui.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@UseCase(
+  name: 'Default',
+  type: IconButton,
+  designLink:
+      'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7337-3250&t=N6qwmLP7MP59ClWB-4',
+)
+Widget buildIconButtonUseCase(BuildContext context) {
+  return IconButton(
+    icon: FontAwesomeIcons.user,
+    onPressed: () {},
+  );
+}
 import 'package:flutter/widgets.dart' hide Icon;
 
 import '../ui.dart';
@@ -52,22 +68,4 @@ class IconButtonState extends State<IconButton> {
       ),
     );
   }
-=======
-import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:groceries_app/ui/ui.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart';
-
-@UseCase(
-  name: 'Default',
-  type: IconButton,
-  designLink:
-      'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7337-3250&t=N6qwmLP7MP59ClWB-4',
-)
-Widget buildIconButtonUseCase(BuildContext context) {
-  return IconButton(
-    icon: FontAwesomeIcons.user,
-    onPressed: () {},
-  );
->>>>>>> 65584a9 (try)
 }
